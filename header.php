a602eb12ddf533c4dbe67980439a881ff9bdaee0
mrewers <mrewers@users.noreply.github.com>
2019-10-02 11:16:57 -0400

    Add footer

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 
         <div class="top-nav-container">
           
-            <?php if ( has_nav_menu('primary') ) { 
+            <?php if ( has_nav_menu( 'primary' ) ) { 
               wp_nav_menu( array(
                   'theme_location' => 'primary'
                 ) );
