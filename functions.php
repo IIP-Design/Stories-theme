d9e8fd3524588d540feb042e914f0d1170a8e25e
mrewers <mrewers@users.noreply.github.com>
2019-11-04 15:08:00 -0500

    Place Iran page script in the footer - to ensure proper loading order

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action( 'wp_enqueue_scripts', 'iran_enqueue');
 
 function iran_enqueue() {
   if ( is_page( 'iran' ) ) {
-    wp_enqueue_script( 'iran', get_stylesheet_directory_uri() . '/includes/stories/iran/iran.js', array(), null, false );
+    wp_enqueue_script( 'iran', get_stylesheet_directory_uri() . '/includes/stories/iran/iran.js', array(), null, true );
     
     wp_enqueue_style( 'iran', get_stylesheet_directory_uri() . '/includes/stories/iran/iran.css', array(), null, false );
   }
