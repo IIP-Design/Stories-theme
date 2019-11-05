30340cea30ccbc8b5a3d1de7b2918ce1d161bb0d
mrewers <mrewers@users.noreply.github.com>
2019-11-05 16:53:14 -0500

    Enqueue animation scripts and styles on 5G page

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
 
+add_action( 'wp_enqueue_scripts', 'fiveg_enqueue');
 add_action( 'wp_enqueue_scripts', 'iran_enqueue');
+
+function fiveg_enqueue() {
+  if ( is_page( '5g' ) ) {
+    wp_enqueue_script( '5g', get_stylesheet_directory_uri() . '/includes/stories/5g/fiveg.js', array(), null, true );
+    
+    wp_enqueue_style( '5g', get_stylesheet_directory_uri() . '/includes/stories/5g/fiveg.css', array(), null, false );
+  }
+};
 
 function iran_enqueue() {
   if ( is_page( 'iran' ) ) {
