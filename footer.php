95d0c503f13facf2bc265ea543776b2a13b6c0af
mrewers <mrewers@users.noreply.github.com>
2019-11-08 13:44:01 -0500

    Replicate state.gov footer

--- footer.php
+++ footer.php
@@ -1,37 +1,9 @@
   </div> <!-- End main-content -->
   
-  <footer>
-    <div class="content-wrap footer-wrap">
+  <footer class="site-footer footer">
+    <div class="footer__content frame">
 
-      <div class="footer-image">
-        <a href="https://state.gov">
-          <img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/images/dos_seal.png">
-        </a>
-      </div> <!-- End footer-image -->
-
-      <div class="footer-content">
-        <div class="footer-content-top">
-        <?php get_template_part( 'partials/callback' ) ?>
-
-        <?php if ( has_nav_menu( 'primary' ) ) { 
-          wp_nav_menu( array(
-            'container' => "",
-            'menu_class' => 'menu-bottom-nav menu-primary',
-            'theme_location' => 'primary'
-          ) );
-        } ?>
-        </div>
-
-        <?php if ( has_nav_menu( 'secondary' ) ) { ?>
-          <div class="footer-content-bottom">
-            <?php wp_nav_menu( array(
-              'container' => "",
-              'menu_class' => 'menu-bottom-nav menu-secondary',
-              'theme_location' => 'secondary'
-            ) ); ?>
-          </div>
-        <?php } ?>
-      </div> <!-- End footer-content -->
+        <?php get_template_part( 'partials/state_footer' ) ?>
 
     </div> <!-- End footer-wrap -->
   </footer>
---
+++ partials/state_footer.php
@@ -0,0 +1,88 @@
+<div class="footer__primary-wrapper">
+  <div class="footer__logo">
+    <a
+      class="footer__logo-image"
+      title="United States Department of State"
+      href="https://www.state.gov"
+    ></a>
+  </div>
+  <div class="footer__primary">
+    <div class="menu-footer-primary-navigation-container">
+      <ul id="menu-footer-primary-navigation" class="menu">
+        <li
+          id="menu-item-15994"
+          class="menu-item menu-item-type-custom menu-item-object-custom menu-item-15994"
+        >
+          <a href="https://www.whitehouse.gov/">White House</a>
+        </li>
+        <li
+          id="menu-item-15995"
+          class="menu-item menu-item-type-custom menu-item-object-custom menu-item-15995"
+        >
+          <a href="https://www.usa.gov/">USA.gov</a>
+        </li>
+        <li
+          id="menu-item-15996"
+          class="menu-item menu-item-type-custom menu-item-object-custom menu-item-15996"
+        >
+          <a href="https://www.stateoig.gov/">Office of the Inspector General</a>
+        </li>
+        <li
+          id="menu-item-67737"
+          class="menu-item menu-item-type-post_type menu-item-object-page menu-item-67737"
+        >
+          <a href="https://www.state.gov/u-s-department-of-state-archive-websites/">Archives</a>
+        </li>
+        <li
+          id="menu-item-15998"
+          class="menu-item menu-item-type-custom menu-item-object-custom menu-item-15998"
+        >
+          <a href="https://register.state.gov/contactus/contactusform">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="footer__aux">
+  <div class="menu-footer-auxiliary-navigation-container">
+    <ul id="menu-footer-auxiliary-navigation" class="menu">
+      <li
+        id="menu-item-54473"
+        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-54473"
+      >
+        <a href="https://www.state.gov/privacy-policy/">Privacy Policy</a>
+      </li>
+      <li
+        id="menu-item-54474"
+        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-54474"
+      >
+        <a href="https://www.state.gov/section-508-accessibility-statement/"
+          >Accessibility Statement</a
+        >
+      </li>
+      <li
+        id="menu-item-54475"
+        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-54475"
+      >
+        <a href="https://www.state.gov/copyright-information/">Copyright Information</a>
+      </li>
+      <li
+        id="menu-item-16003"
+        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-16003"
+      >
+        <a href="https://foia.state.gov/">FOIA</a>
+      </li>
+      <li
+        id="menu-item-54477"
+        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-54477"
+      >
+        <a
+          href="https://www.state.gov/key-topics-office-of-civil-rights/eeo-no-fear-act-whistleblower-protection-acts/"
+          >No FEAR Act</a
+        >
+      </li>
+    </ul>
+  </div>
+</div>
+<!-- .footer -->
