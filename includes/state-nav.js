4bc1ee36228053c48b78fc3277be09e0edf5bf8f
mrewers <mrewers@users.noreply.github.com>
2019-11-13 10:45:41 -0500

    Activate bureaus submenu on click

--- includes/state-nav.js
+++ includes/state-nav.js
@@ -10,17 +10,17 @@ const makeNavVisible = item => {
 
   if (overlay && overlay[0].classList.contains('is-visible')) {
     if (subItemsArray) {
-      subItemsArray.forEach(item => {
-        if (item.classList.contains('nav__secondary-nav')) {
-          item.classList.remove('is-visible');
-          item.classList.add('is-hidden');
-        } else {
-          item.classList.remove('selected');
-        }
-      });
+      // subItemsArray.forEach(item => {
+      //   if (item.classList.contains('nav__secondary-nav')) {
+      //     item.classList.remove('is-visible');
+      //     item.classList.add('is-hidden');
+      //   } else {
+      //     item.classList.remove('selected');
+      //   }
+      // });
     }
 
-    overlay[0].classList.remove('is-visible');
+    // overlay[0].classList.remove('is-visible');
   } else if (overlay && !overlay[0].classList.contains('is-visible')) {
     overlay[0].classList.add('is-visible');
 
@@ -40,5 +40,66 @@ const makeNavVisible = item => {
 if (menuItems) {
   menuItemsArray.forEach(item => {
     return item.addEventListener('click', () => makeNavVisible(item));
+  });
+}
+
+const bureauList = document.querySelector('.nav__bureaus-offices-list');
+
+const activateSubmenu = item => {
+  const els = [...item.children];
+  if (els) {
+    els.forEach(el => {
+      if (el.tagName.toLowerCase() === 'ul') {
+        if (el.classList.contains('is-hidden')) {
+          el.classList.remove('is-hidden', 'is-cloaked');
+          el.setAttribute('style', 'display: block');
+        }
+      } else if (el.tagName.toLowerCase() === 'button') {
+        if (!el.classList.contains('selected')) {
+          el.classList.add('selected');
+        }
+      }
+    });
+  }
+};
+
+const deactivateSubmenu = item => {
+  const els = [...item.children];
+  if (els) {
+    els.forEach(el => {
+      if (el.tagName.toLowerCase() === 'ul') {
+        if (!el.classList.contains('is-hidden')) {
+          el.classList.add('is-hidden', 'is-cloaked');
+          el.setAttribute('style', '');
+        }
+      } else if (el.tagName.toLowerCase() === 'button') {
+        if (el.classList.contains('selected')) {
+          el.classList.remove('selected');
+        }
+      }
+    });
+  }
+};
+
+const toggleSubmenu = bureau => {
+  // Set all menu items as inactive
+  const allMenuItems = [...bureau.parentNode.children];
+  allMenuItems.forEach(item => {
+    item.classList.add('is-inactive');
+    deactivateSubmenu(item);
+  });
+
+  // Reset selected item as active
+  bureau.classList.remove('is-inactive');
+
+  // Active selected item submenu
+  activateSubmenu(bureau);
+};
+
+if (bureauList) {
+  const bureaus = [...bureauList.children];
+
+  bureaus.forEach(bureau => {
+    return bureau.addEventListener('click', () => toggleSubmenu(bureau));
   });
 }
